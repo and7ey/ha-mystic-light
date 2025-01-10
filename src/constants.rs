2fbca6dd7cc1c43f1c07239105a7cc40809ffffb
and7ey <and7ey@gmail.com>
2025-01-10 10:15:12 +0300

    remove cinema wording

--- src/constants.rs
+++ src/constants.rs
@@ -1,2 +1,2 @@
-pub const SERVICE_NAME: &str = "mystic_light_browser_cinema";
-pub const SERVICE_DISPLAY_NAME: &str = "Mystic Light Browser Cinema";
+pub const SERVICE_NAME: &str = "mystic_light_ha";
+pub const SERVICE_DISPLAY_NAME: &str = "Mystic Light HA";
